0344682cfd10a37d5c947c738f8406117f68a185
Ivanna <ivanna.sak@gmail.com>
2020-04-21 14:37:25 -0700

    check if malloc is not NULL

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: isak <isak@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/21 14:20:27 by isak              #+#    #+#             */
-/*   Updated: 2020/04/21 14:22:04 by isak             ###   ########.fr       */
+/*   Updated: 2020/04/21 14:34:21 by isak             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,5 +23,12 @@
 
 void    *ft_calloc(size_t count, size_t size)
 {
+	size_t	res_size;
+	void	*p;
 
+	res_size = size * count;
+	if (!(p = malloc(res_size)))
+		return (0);
+	ft_memset(p, 0, res_size);
+	return (p);
 }
