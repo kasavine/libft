fd0b773dab042aba4773d7f4a72d79201025b35a
Ivanna <ivanna.sak@gmail.com>
2020-04-21 16:50:55 -0700

    updated with norms

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: isak <isak@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/21 14:20:27 by isak              #+#    #+#             */
-/*   Updated: 2020/04/21 14:34:21 by isak             ###   ########.fr       */
+/*   Updated: 2020/04/21 15:06:08 by isak             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,14 +21,14 @@
 
 #include "libft.h"
 
-void    *ft_calloc(size_t count, size_t size)
+void		*ft_calloc(size_t count, size_t size)
 {
 	size_t	res_size;
 	void	*p;
 
 	res_size = size * count;
 	if (!(p = malloc(res_size)))
-		return (0);
+		return (NULL);
 	ft_memset(p, 0, res_size);
 	return (p);
 }
