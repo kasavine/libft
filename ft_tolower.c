f25fbeb91a5407cb776e219673de3de29fa96c44
Ivanna <ivanna.sak@gmail.com>
2020-04-21 14:26:25 -0700

    ft_to functions

---
+++ ft_tolower.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_tolower.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: isak <isak@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/02/28 12:33:40 by isak              #+#    #+#             */
+/*   Updated: 2020/04/21 10:23:15 by isak             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+**	tolower -- upper case to lower case letter conversion
+**
+**	The tolower() function converts an upper-case letter to the corresponding
+**		lower-case letter.  The argument must be representable as an unsigned
+**		char or the value of EOF.
+**
+**	If the argument is an upper-case letter, the tolower() function returns
+**		the corresponding lower-case letter if there is one; otherwise, the
+**		argument is returned unchanged.
+*/
+
+#include "libft.h"
+
+int		ft_tolower(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		c = c + ('a' - 'A');
+	return (c);
+}
---
+++ ft_toupper.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_toupper.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: isak <isak@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/02/28 12:32:33 by isak              #+#    #+#             */
+/*   Updated: 2020/04/21 10:26:16 by isak             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+**	toupper -- lower case to upper case letter conversion
+**
+**	The toupper() function converts a lower-case letter to the corresponding
+**		upper-case letter.  The argument must be representable as an unsigned
+**		char or the value of EOF.
+**
+**	If the argument is a lower-case letter, the toupper() function returns
+**		the corresponding upper-case letter if there is one; otherwise, the
+**		argument is returned unchanged.
+*/
+
+#include "libft.h"
+
+int		ft_toupper(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		c = c - ('a' - 'A');
+	return (c);
+}
