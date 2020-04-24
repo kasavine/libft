4157394a119f6b8840767cfc980c550368ba27df
Ivanna <ivanna.sak@gmail.com>
2020-04-23 20:06:56 -0700

    updated prototype

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: isak <isak@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/02 11:52:57 by isak              #+#    #+#             */
-/*   Updated: 2020/04/21 16:13:20 by isak             ###   ########.fr       */
+/*   Updated: 2020/04/23 20:05:47 by isak             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,27 +25,27 @@
 
 #include "libft.h"
 
-char		*ft_strnstr(const char *haystack, const char *needle, size_t len)
+char		*ft_strnstr(const char *h, const char *n, size_t len)
 {
-	char	*h;
+	char	*h_new;
 	size_t	needle_len;
 	size_t	j;
 	size_t	i;
 
-	h = (char *)haystack;
-	needle_len = ft_strlen(needle);
+	h_new = (char *)h;
+	needle_len = ft_strlen(n);
 	if (!(needle_len))
-		return (h);
-	if (ft_strlen(haystack) < needle_len || len < needle_len)
+		return (h_new);
+	if (ft_strlen(h) < needle_len || len < needle_len)
 		return (NULL);
 	i = 0;
-	while (h[i] && i <= len - needle_len)
+	while (h_new[i] && i <= len - needle_len)
 	{
 		j = 0;
-		while (needle[j] && needle[j] == h[i + j])
+		while (n[j] && n[j] == h_new[i + j])
 			j++;
 		if (j == needle_len)
-			return (&h[i]);
+			return (&h_new[i]);
 		i++;
 	}
 	return (NULL);
