434a951d4c81594e734f5633b8aaf38b848cb523
Ivanna <ivanna.sak@gmail.com>
2020-05-15 10:13:51 -0700

    fixed type casting

--- ft_calloc.c
+++ ft_calloc.c
@@ -14,12 +14,11 @@
 
 void		*ft_calloc(size_t count, size_t size)
 {
-	size_t	res_size;
-	void	*p;
+	char	*p;
 
-	res_size = size * count;
-	if (!(p = malloc(res_size)))
+	p = malloc(count * size);
+	if (!(p))
 		return (NULL);
-	ft_memset(p, 0, res_size);
+	ft_memset(p, 0, count * size);
 	return (p);
 }
